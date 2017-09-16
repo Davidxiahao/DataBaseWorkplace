1e5cf9beba0b7d43100f82e0165bfaf7dff13b9a
Hank Zhang <westrainzxh@gmail.com>
2017-09-16 16:36:40 +0800

    add interface for word segmentation

---
+++ src/main/java/com/xiahao/lib/WordSegmentationUtil.java
@@ -0,0 +1,18 @@
+package com.xiahao.lib;
+
+import java.util.Set;
+
+/**
+ * Created by HankZhang on 2017/9/16.
+ */
+public class WordSegmentationUtil {
+    /***
+     *
+     * @param targetStr webOrigins to be segmented.
+     * @param helperStr package name from codeOrigins to help the segmentation.
+     * @return
+     */
+    public static Set<String> segmentWord(String targetStr, String helperStr) {
+        return null;
+    }
+}
